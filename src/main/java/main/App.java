aca9124ea7b76414782d84583e1c431b9345c778
gettyreal <getty.fafano@gmail.com>
2025-04-06 20:23:02 +0200

    main method resolved

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -7,7 +7,7 @@ import javafx.stage.Stage;
  * JavaFX App
  */
 public class App extends Application{
-    void main(String[] args) {
+    public static void main(String[] args) {
         launch(args);
     }
 
